38d79d366e5a3cd282730433df061ddcc57dbd38
PedroVPrado <pedrovalencaprado@gmail.com>
2022-01-19 19:58:42 -0300

    Add new Lambda URL

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var callAPI = (firstName,lastName)=>{
         redirect: 'follow'
     };
     // make API call with parameters and use promises to get response
-    fetch("https://hft1vuw3xi.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+    fetch("https://qurqbuai16.execute-api.us-east-1.amazonaws.com", requestOptions)
     .then(response => response.text())
     .then(result => alert(JSON.parse(result).body))
     .catch(error => console.log('error', error));
